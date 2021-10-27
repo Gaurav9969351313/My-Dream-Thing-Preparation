07de484cb4a4a91388187661f4f9e510aa4c30a3
mahindra mahindra <mahindraanalyticspath@gmail.com>
2021-10-27 11:15:32 +0530

    added resume V7

--- python-email-sender/app.py
+++ python-email-sender/app.py
@@ -21,8 +21,8 @@ for x in xrange(len(first_column)):
       toaddr = to_add
 
       email_subject="Sr. Full Stack Develoer (Resume - Gaurav Talele)"
-      resume_path="./Resume - Gaurav Talele.pdf"
-      filename = "Resume - Gaurav Talele.pdf"
+      resume_path="./Resume - GauravTalele.pdf"
+      filename = "Resume - GauravTalele.pdf"
 
       msg = MIMEMultipart('alternative')
       msg['From'] = fromaddr
@@ -32,41 +32,56 @@ for x in xrange(len(first_column)):
       body = "Dear Hiring Manager, Please find attached resume."
 
       text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
-      html = """\
+      html = """
       <html>
-        <head></head>
-        <body>
-          
-          <h4>Dear Hiring Manager,</h4> 
-          <h6>I hope you’re doing well. <h6>
+  <head></head>
+  <body style=" font-family: Arial, Helvetica, sans-serif; font-size: 22px">
+    
+    <h4>Dear Hiring Manager,</h4> 
+    <h6>I hope you are doing well. <h6>
 
-          I am writing to express my strong interest in being the next Full Stack Engineer at your organisation.
-          <br>
-          As an ambitious professional with experience in Angular, Angular Material, Spring Boot, Core Java, Microservices, Axon, Oracle, MongoDB, Jasper Reports,
-      Docker, Docker Compose, AWS. I believe that I would make an excellent addition to your team. I am currently working as a Full Stack Engineer in 
-      Reserve Bank Of Information Technologies Pvt Ltd.
-      <br>
-      <br>
-      I have attached my CV hoping that you will consider me for the place in your organisation, should you need more details or references I can supply those at your convenience. Otherwise I would like to have an interview and talk more about being new employee for your esteemed Organisation. Please review.
+    <p style="font-weight: normal; line-height: 24px">
+    	I am writing to express my strong interest in being the next Full Stack Developer at your organisation.
+	    <br>
+	    As an ambitious professional with <span style="font-size: 16px; font-weight:bold"> 4.9 Years </span> of experience in <span style="font-size: 16px; font-weight:bold">Angular, Angular Material, Spring Boot, Core Java, Microservices, Axon, Oracle, MongoDB, Jasper Reports, Docker, Docker Compose, AWS.</span> 
+		I believe that I would make an excellent addition to your team. I am currently working as a Full Stack Developer in Reserve Bank Of Information Technologies Pvt Ltd.
+		<br>
+		<br>
+		I have attached my CV hoping that you will consider me for the place in your organisation, should you need more details or references I can supply those at your convenience. Otherwise I would like to have an interview and talk more about being new employee for your esteemed Organisation. Please review.
 
-      <br>
-      <br>
-      Thank you so much for your time and consideration..:)
-      <br>
+		<br>
+		<br>
+		Thank you so much for your time and consideration..:)
+		<br>
+	</p>
 
-      <p>Current CTC: 12.76 LPA<p>
-      <p>Notice Period: 45 days.</p>
+	<table style="font-weight: normal; line-height: 24px;" border=1>
+		<tr>
+			<td style="font-weight: bold;">Current CTC</td>
+			<td>12 LPA</td>
+		</tr>
+		<tr>
+			<td style="font-weight: bold;">Location Preferences</td>
+			<td>Mumbai, Pune, Banglore</td>
+		</tr>
+		<tr>
+			<td style="font-weight: bold;">Notice Period</td>
+			<td>45 Days</td>
+		</tr>
+	</table>
 
-      <p>Also It would be great if you foreword me the detailed JD.</p>
-      <br>
-      <br>
+	<p style="font-weight: normal; line-height: 22px">Also It would be great if you foreword me the detailed JD.</p>
+	<br>
 
-      With Best Regards,<br>
-      Mr. Gaurav Y Talele. <br>
-      [M]: garry.talele@gmail.com<br>
-      [H]: +91 9969351313.<br>
-        </body>
-      </html>
+	<p style="line-height: 24px">With Best Regards,<br></p>
+	<p style="line-height: 22px; font-weight: normal;">	
+		Mr. Gaurav Y Talele. <br>
+		[M]: garry.talele@gmail.com<br>
+		[H]: +91 9969351313.<br>
+	</p>
+
+  </body>
+</html>
       """
 
       part1 = MIMEText(text, 'plain')
